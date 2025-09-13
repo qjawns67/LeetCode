642a322d209b3b39184405c98e6e66bd1af2b019
qjawns67 <qjawns67@gmail.com>
2025-09-13 22:29:19 +0900

    Time: 4 ms (90%), Space: 12.3 MB (81.35%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,33 +1,12 @@
-#
-# @lc app=leetcode id=9 lang=python
-#
-# [9] Palindrome Number
-#
-
-# @lc code=start
 class Solution(object):
     def isPalindrome(self, x):
         """
         :type x: int
         :rtype: bool
         """
-        #without converting Int to Str
-        reverse_num=0;
-        if x<0:
+        a=str(x);
+        if a!=a[::-1]:
             return False;
-        elif x==0:
-            return True;
         else:
-            if x%10==0:
-                return False;
-            else:
-                while x>reverse_num:
-                    reverse_num*=10;
-                    reverse_num+=x%10;
-                    x//=10;
-        if x==reverse_num or x==reverse_num//10:
             return True;
-        else:
-            return False;
-# @lc code=end
-
+        
