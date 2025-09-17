302cc036ffa29fa22e841828886fd0c4ee7ebf61
qjawns67 <qjawns67@gmail.com>
2025-09-17 23:19:38 +0900

    Time: 18 ms (83.89%), Space: 16.1 MB (64.08%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=49 lang=python
+#
+# [49] Group Anagrams
+#
+
+# @lc code=start
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        map={};
+        final=[];
+        for i in range(len(strs)):
+            sorted_str="".join(sorted(strs[i]));
+            if not sorted_str in map:
+                tmp=[strs[i]];
+                map[sorted_str]=tmp;
+            else:
+                map[sorted_str].append(strs[i]);
+        return map.values();
+
+# @lc code=end
+
