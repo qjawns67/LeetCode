b634c0d02f2fda3a6fbd0698064cd580509e1712
qjawns67 <qjawns67@gmail.com>
2025-09-18 17:32:50 +0900

    Time: 8 ms (74.28%), Space: 14 MB (95.4%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,7 +3,7 @@
 #
 # [347] Top K Frequent Elements
 #
-
+import heapq;
 # @lc code=start
 class Solution(object):
     def topKFrequent(self, nums, k):
@@ -13,16 +13,18 @@ class Solution(object):
         :rtype: List[int]
         """
         tmp={};
-        final=[];
         for i in nums:
             if not i in tmp:
                 tmp[i]=1;
             else:
                 tmp[i]+=1;
-        final_list=sorted(tmp.items(), key=lambda x:x[1], reverse=True)
-        for i in range(k):
-            final.append(final_list[i][0]);
-        return final;
+        heap=[];
+        for i in tmp.keys():
+            heapq.heappush(heap,(tmp[i],i));
+            if len(heap)>k:
+                heapq.heappop(heap);
+        
+        return [heapq.heappop(heap)[1] for _ in range(k)];
 
 
 # @lc code=end
